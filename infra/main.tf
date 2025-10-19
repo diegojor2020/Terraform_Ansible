5550d4cdd352afd72947ee3110d7ddae5d48a815
Seu Nome <seu-email@exemplo.com>
2025-10-18 23:47:54 -0300

    Adicionando novos arquivos Terraform e Ansible

---
+++ infra/main.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.regiao_aws
+}
+
+resource "aws_launch_template" "maquina" {
+  image_id           = "ami-03d5c68bab01f3496"
+  instance_type = var.instancia
+  key_name = var.chave
+  tags = {
+    Name = "Terraform Ansible Python"
+  }
+  security_group_names = [var.grupoDeSeguranca]
+  user_data = var.producao ? file("ansible.sh") : ""
+}
+
+resource "aws_key_pair" "chaveSSH" {
+  key_name = var.chave
+  public_key = file("${var.chave}.pub") 
+}
+
+resource "aws_autoscaling_group" "grupo_de_maquinas" {
+  availability_zones = ["${var.regiao_aws}a", "${var.regiao_aws}b"]
+  name = var.nomeGrupo
+  max_size = var.maximo
+  min_size = var.minimo
+
+  launch_template {
+    id = aws_launch_template.maquina.id
+    version = "$Latest"
+  }
+  target_group_arns = var.producao ? [aws_lb_target_group.alvoLoadBalancer[0].arn] : []
+  depends_on = [aws_launch_template.maquina]
+}
+
+resource "aws_default_subnet" "subnet_1" {
+  availability_zone = "${var.regiao_aws}a"
+  
+}
+
+
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = "${var.regiao_aws}b"
+  
+}
+
+resource "aws_lb" "loadBalancerProducao" {
+  internal = false
+  subnets = [aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id]
+  count = var.producao ? 1 : 0
+  
+}
+
+
+resource "aws_lb_target_group" "alvoLoadBalancer" {
+  name = "maquinasAlvo"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+  count = var.producao ? 1 : 0
+  
+}
+
+resource "aws_default_vpc" "default" {
+
+  
+}
+
+
+resource "aws_lb_listener" "entradaLoadBalancer" {
+  load_balancer_arn = aws_lb.loadBalancerProducao[0].arn
+  port = "8000"
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alvoLoadBalancer[0].arn
+  }
+  count = var.producao ? 1 : 0
+  
+}
+
+resource "aws_autoscaling_policy" "escala-Producao" {
+  name = "terraform-escala"
+  autoscaling_group_name = var.nomeGrupo
+  policy_type = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 50.0
+  }
+  count = var.producao ? 1 : 0
+  
+}
---
+++ infra/variables.tf
@@ -0,0 +1,33 @@
+variable "regiao_aws" {
+  type = string
+}
+variable "chave" {
+    type = string
+}
+variable "instancia" {
+  type = string
+}
+
+variable "grupoDeSeguranca" {
+  type = string
+}
+
+
+variable "minimo" {
+  type = number
+  
+}
+
+variable "maximo" {
+  type = number
+  
+}
+variable "nomeGrupo" {
+  type = string
+}
+
+
+variable "producao" {
+  type = bool
+  default = false
+}
